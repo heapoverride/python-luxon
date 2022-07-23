8f2a92ccce99ad10b46e33e07275218a7bc2b2af
Unreal Security <arran.bishop89@aol.com>
2022-07-23 14:43:40 +0300

    Added proper Google style Python docstrings to all public methods of Tag base class and Text element

--- luxon/html/__init__.py
+++ luxon/html/__init__.py
@@ -1,10 +1,17 @@
 from __future__ import annotations
 from typing import Any, Callable
 
-# Base class for all HTML tags
 class Tag:
+    """Base class for all HTML elements\n 
+    All other elements inherit from this base class
+    """
     def __init__(self, name: str):
-        self.__name: str = name
+        """Construct a HTML tag
+        
+        Args:
+            name (str): Tag name
+        """
+        self.__name: str = name.lower() if type(name) == str else name
         self.__attributes: dict[str, Any] = {}
         self.__classes: list[str] = []
         self.__styles: dict[str, str] = {}
@@ -18,18 +25,25 @@ class Tag:
         self.hidden: bool = False
         self.parent: Tag = None
 
-    # Indent a line of HTML source code
-    def __indent(self, depth: int) -> str:
-        return "    " * depth
-
-    # Set text and mark this element a text element
     def set_text(self, text: Any):
+        """Set displayed text and mark this element a text element
+
+        Args:
+            text (Any): String value or other type that can be converted to string
+
+        Returns:
+            self
+        """
         self.is_text = True
         self.text = text
         return self
 
-    # Add child elements
     def add(self, *tags: Tag|list[Tag|str]|str):
+        """Add child elements to this element
+
+        Returns:
+            self
+        """
         for tag in tags:
             if type(tag) == str:
                 self.add(Text(tag))
@@ -38,28 +52,49 @@ class Tag:
             else:
                 tag.parent = self
                 self.__tags.append(tag)
-
         return self
 
-    # Set child elements
     def set_body(self, *tags: Tag|list[Tag|str]|str):
+        """Set this element's child elements
+
+        Returns:
+            self
+        """
         self.remove_all()
         self.add(*tags)
         return self
 
-    # Remove a child tag
     def remove(self, tag: Tag):
+        """Removes a child element from this element
+
+        Args:
+            tag (Tag): Child element to remove
+
+        Returns:
+            self
+        """
         if tag in self.__tags:
             self.__tags.remove(tag)
         return self
 
-    # Remove all children tags
     def remove_all(self):
+        """Remove all child elements from this element
+
+        Returns:
+            self
+        """
         self.__tags.clear()
         return self
 
-    # Insert tags at index
     def insert(self, index: int, *tags: Tag|list[Tag|str]|str):
+        """Insert child elements at specific index
+
+        Args:
+            index (int): Insert elements before this index
+
+        Returns:
+            self
+        """
         for tag in tags[::-1]:
             if type(tag) == str:
                 self.__tags.insert(index, Text(tag))
@@ -69,8 +104,15 @@ class Tag:
                 self.__tags.insert(index, tag)
         return self
 
-    # Remove all children tags where lambda expression is true
     def remove_where(self, func: Callable[[Tag], bool]):
+        """Remove all child elements where lambda expression or named function returns True
+
+        Args:
+            func (Callable[[Tag], bool]): Lambda expression or named function
+
+        Returns:
+            self
+        """
         i = 0
         while i < len(self.__tags):
             if func(self.__tags[i]):
@@ -79,87 +121,188 @@ class Tag:
             i += 1
         return self
 
-    # Set attribute with optional value
     def set(self, attribute: str, value: Any = True):
+        """Set attribute with optional value
+
+        Args:
+            attribute (str): Attribute name
+            value (Any, optional): Attribute value
+
+        Returns:
+            self
+        """
         self.__attributes[attribute] = value
         return self
 
-    # Get attribute value by it's name
     def get(self, attribute: str) -> Any|None:
+        """Get attribute value by it's name
+
+        Args:
+            attribute (str): Attribute name
+
+        Returns:
+            Any|None
+        """
         if attribute in self.__attributes:
             return self.__attributes[attribute]
         return None
 
-    # Unset attribute by name
     def unset(self, *attributes: str):
+        """Unset attribute by it's name
+
+        Returns:
+            self
+        """
         for attribute in attributes:
             if attribute in self.__attributes:
                 del self.__attributes[attribute]
         return self
 
-    # Set class list
     def set_classes(self, *class_names: str):
+        """Set class list
+
+        Returns:
+            self
+        """
         self.__classes = list(class_names)
         return self
 
-    # Add class to class list
     def add_class(self, class_name: str):
+        """Add class to class list
+
+        Args:
+            class_name (str): Class name
+
+        Returns:
+            self
+        """
         if class_name not in self.__classes:
             self.__classes.append(class_name)
         return self
 
-    # Unset class from class list
     def unset_class(self, class_name: str):
+        """Unset class from class list
+
+        Args:
+            class_name (str): Class name
+
+        Returns:
+            self
+        """
         if class_name in self.__classes:
             self.__classes.remove(class_name)
         return self
 
-    # Set 'id' attribute
     def set_id(self, id: str):
+        """Set id attribute's value
+
+        Args:
+            id (str): ID
+
+        Returns:
+            self
+        """
         self.set("id", id)
         return self
 
-    # Get value of 'id' attribute
     def get_id(self):
+        """Get id attribute's value
+
+        Returns:
+            Any|None
+        """
         return self.get("id")
 
-    # Set 'name' attribute
     def set_name(self, name: str):
+        """Set name attribute's value
+
+        Args:
+            name (str): Name
+
+        Returns:
+            self
+        """
         self.set("name", name)
         return self
 
-    # Get value of 'name' attribute
     def get_name(self):
+        """Get name attribute's value
+
+        Returns:
+            Any|None
+        """
         return self.get("name")
 
-    # Set 'value' attribute
     def set_value(self, value: Any):
+        """Set value attribute's value
+
+        Args:
+            value (str): Value
+
+        Returns:
+            self
+        """
         self.set("value", value)
         return self
 
-    # Get value of 'value' attribute
     def get_value(self):
+        """Get value attribute's value
+
+        Returns:
+            Any|None
+        """
         return self.get("value")
 
-    # Set style property and it's value
     def set_style(self, property: str, value: str):
+        """Set style property
+
+        Args:
+            property (str): Property name
+            value (str): Property value
+
+        Returns:
+            self
+        """
         self.__styles[property] = value
         return self
 
-    # Get style by it's property name
     def get_style(self, property: str) -> str|None:
+        """Get style property's value
+
+        Args:
+            property (str): Property name
+
+        Returns:
+            str|None
+        """
         if property in self.__styles:
             return self.__styles[property]
         return None
 
-    # Unset style property
     def unset_style(self, property: str):
+        """Unset stype property
+
+        Args:
+            property (str): Property name
+
+        Returns:
+            self
+        """
         if property in self.__styles:
             del self.__styles[property]
         return self
 
-    # Find a single tag
     def find(self, func: Callable[[Tag], bool], recurse: bool = True, max_depth: int = None) -> Tag|None:
+        """Find the first element where lambda expression or named function returns `True`
+
+        Args:
+            func (Callable[[Tag], bool]): Lambda expression or named function
+            recurse (bool, optional): Use recursion. Defaults to `True`.
+            max_depth (int, optional): Max recursion depth. Defaults to `None`.
+
+        Returns:
+            Tag|None: Found element or None if no element was found
+        """
         if max_depth != None: max_depth -= 1
 
         for tag in self.__tags:
@@ -180,100 +323,195 @@ class Tag:
                 if recurse and (max_depth == None or max_depth > -1): 
                     Tag.__find_all(_tag, func, result, recurse=recurse, max_depth=max_depth)
 
-    # Find tags
     def find_all(self, func: Callable[[Tag], bool], recurse: bool = True, max_depth: int = None) -> list[Tag]:
+        """Find all elements where lambda expression or named function returns `True`
+
+        Args:
+            func (Callable[[Tag], bool]): Lambda expression or named function
+            recurse (bool, optional): Use recursion. Defaults to `True`.
+            max_depth (int, optional): Max recursion depth. Defaults to `None`.
+
+        Returns:
+            list[Tag]: List of found elements
+        """
         result = []
         Tag.__find_all(self, func, result, recurse=recurse, max_depth=max_depth)
         return result
 
-    # Find tag by ID recursively
     def find_by_id(self, id: str):
+        """Find element by id (attribute) recursively
+
+        Args:
+            id (str): ID
+
+        Returns:
+            Tag|None: Found element or None if no element was found
+        """
         return self.find(lambda tag: tag.get_id() == id)
 
-    # Find tag by name attribute (recursively?)
     def find_by_name(self, name: str, recurse: bool = True, max_depth: int = None):
+        """Find element by name (attribute) recursively
+
+        Args:
+            name (str): Name
+            recurse (bool, optional): Use recursion. Defaults to `True`.
+            max_depth (int, optional): Max recursion depth. Defaults to `None`.
+
+        Returns:
+            Tag|None: Found element or None if no element was found
+        """
         return self.find(lambda tag: tag.get_name() == name, 
             recurse=recurse, 
             max_depth=max_depth)
 
-    # Find tags by name attribute (recursively?)
     def find_all_by_name(self, name: str, recurse: bool = True, max_depth: int = None):
+        """Find all elements by name (attribute) recursively
+
+        Args:
+            name (str): Name
+            recurse (bool, optional): Use recursion. Defaults to `True`.
+            max_depth (int, optional): Max recursion depth. Defaults to `None`.
+
+        Returns:
+            list[Tag]: List of found elements
+        """
         return self.find_all(lambda tag: tag.get_name() == name, 
             recurse=recurse, 
             max_depth=max_depth)
 
-    # Find tag by tag name (recursively?)
     def find_by_tagname(self, tagname: str, recurse: bool = True, max_depth: int = None):
+        """Find element by tag name recursively
+
+        Args:
+            tagname (str): Tag name
+            recurse (bool, optional): Use recursion. Defaults to `True`.
+            max_depth (int, optional): Max recursion depth. Defaults to `None`.
+
+        Returns:
+            Tag|None: Found element or None if no element was found
+        """
         return self.find(lambda tag: tag.name == tagname, 
             recurse=recurse, 
             max_depth=max_depth)
 
-    # Find tags by tag name (recursively?)
     def find_all_by_tagname(self, tagname: str, recurse: bool = True, max_depth: int = None):
+        """Find all elements by tag name recursively
+
+        Args:
+            tagname (str): Name
+            recurse (bool, optional): Use recursion. Defaults to `True`.
+            max_depth (int, optional): Max recursion depth. Defaults to `None`.
+
+        Returns:
+            list[Tag]: List of found elements
+        """
         return self.find_all(lambda tag: tag.name == tagname, 
             recurse=recurse, 
             max_depth=max_depth)
 
-    # Find tag by type (recursively?)
     def find_by_type(self, tag_type: type, recurse: bool = True, max_depth: int = None):
+        """Find element by type recursively
+
+        Args:
+            tag_type (type): Tag type (e.g. Div)
+            recurse (bool, optional): Use recursion. Defaults to `True`.
+            max_depth (int, optional): Max recursion depth. Defaults to `None`.
+
+        Returns:
+            Tag|None: Found element or None if no element was found
+        """
         return self.find(lambda tag: type(tag) == tag_type, 
             recurse=recurse, 
             max_depth=max_depth)
 
-    # Find tags by type (recursively?)
     def find_all_by_type(self, tag_type: type, recurse: bool = True, max_depth: int = None):
+        """Find all elements by type recursively
+
+        Args:
+            tag_type (type): Tag type (e.g. Div)
+            recurse (bool, optional): Use recursion. Defaults to `True`.
+            max_depth (int, optional): Max recursion depth. Defaults to `None`.
+
+        Returns:
+            list[Tag]: List of found elements
+        """
         return self.find_all(lambda tag: type(tag) == tag_type, 
             recurse=recurse, 
             max_depth=max_depth)
 
-    # Find tag by class name (recursively?)
     def find_by_class(self, *class_name: str, recurse: bool = True, max_depth: int = None):
+        """Find element by one or more class names recursively
+
+        Args:
+            *class_name (str): Class name(s)
+            recurse (bool, optional): Use recursion. Defaults to `True`.
+            max_depth (int, optional): Max recursion depth. Defaults to `None`.
+
+        Returns:
+            Tag|None: Found element or None if no element was found
+        """
         return self.find(lambda tag: all(item in tag.classes for item in class_name), 
             recurse=recurse, 
             max_depth=max_depth)
 
-    # Find tags by class name (recursively?)
     def find_all_by_class(self, *class_name: str, recurse: bool = True, max_depth: int = None):
-        #return self.find_all(lambda tag: class_name in tag.classes)
+        """Find elements by one or more class names recursively
+
+        Args:
+            *class_name (str): Class name(s)
+            recurse (bool, optional): Use recursion. Defaults to `True`.
+            max_depth (int, optional): Max recursion depth. Defaults to `None`.
+
+        Returns:
+            list[Tag]: List of found elements
+        """
         return self.find_all(lambda tag: all(item in tag.classes for item in class_name), 
             recurse=recurse, 
             max_depth=max_depth)
 
-    # Find tag by attribute value (recursively?)
     def find_by_attribute(self, attribute: str, value: Any = True, recurse: bool = True, max_depth: int = None):
+        """Find element by attribute (and optionally it's value)
+
+        Args:
+            attribute (str): Attribute name
+            value (str, optional): Attribute value
+            recurse (bool, optional): Use recursion. Defaults to `True`.
+            max_depth (int, optional): Max recursion depth. Defaults to `None`.
+
+        Returns:
+            Tag|None: Found element or None if no element was found
+        """
         return self.find(lambda tag: tag.get(attribute) == value if value != True else tag.get(attribute) != None, 
             recurse=recurse, 
             max_depth=max_depth)
 
-    # Find tags by attribute value (recursively?)
     def find_all_by_attribute(self, attribute: str, value: Any = True, recurse: bool = True, max_depth: int = None):
+        """Find elements by attribute (and optionally it's value)
+
+        Args:
+            attribute (str): Attribute name
+            value (str, optional): Attribute value
+            recurse (bool, optional): Use recursion. Defaults to `True`.
+            max_depth (int, optional): Max recursion depth. Defaults to `None`.
+
+        Returns:
+            list[Tag]: List of found elements
+        """
         return self.find_all(lambda tag: tag.get(attribute) == value if value != True else tag.get(attribute) != None, 
             recurse=recurse, 
             max_depth=max_depth)
 
-    # Escape HTML code if escape option is set
-    def __escape(self, input: str, force: bool = False) -> str:
-        replaces = (
-            ("\"", "\\\""),
-            ("<", "&lt;"),
-            (">", "&gt;")
-        )
-
-        if self.escape or force:
-            for old, new in replaces:
-                input = input.replace(old, new)
-
-        return input
-
-    # Call a method with this instance as the first argument
     def call(self, func: Callable[[Tag], None]):
+        """Call a lambda expression or named method with this element as the first argument
+
+        Args:
+            func (Callable[[Tag], None]): Lambda expression or named method
+
+        Returns:
+            self
+        """
         func(self)
         return self
-
-    # This method is called before HTML 
-    # source code is generated and can be overloaded
-    def update(self):
-        pass
         
     def __html(self, pretty: bool = False, depth: int = 0, extend: bool = True) -> str:
         result: str = ""
@@ -344,9 +582,37 @@ class Tag:
 
         return result
 
-    # Produce HTML source code
     def html(self, pretty: bool = False):
+        """Generate this element's HTML source code
+
+        Args:
+            pretty (bool, optional): Pretty print the source code. Defaults to False.
+
+        Returns:
+            str: Generated HTML source code
+        """
         return self.__html(pretty, depth=0)
+
+    def __indent(self, depth: int) -> str:
+        return "    " * depth
+
+    def __escape(self, input: str, force: bool = False) -> str:
+        replaces = (
+            ("\"", "\\\""),
+            ("<", "&lt;"),
+            (">", "&gt;")
+        )
+
+        if self.escape or force:
+            for old, new in replaces:
+                input = input.replace(old, new)
+
+        return input
+
+    # This method is called before HTML 
+    # source code is generated and can be overloaded
+    def update(self):
+        pass
 
     # Overload slice/array access operator
     def __getitem__(self, slice: slice) -> Tag|list[Tag]:
@@ -371,16 +637,23 @@ class Tag:
     # when this tag is printed or converted to string using str()
     def __str__(self):
         return self.html()
+
     def __repr__(self): 
         return self.__str__()
 
-# Text element
 class Text(Tag):
+    """Text element"""
     def __init__(self, text: Any):
+        """Construct a text element
+
+        Args:
+            text (Any): String value or other type that can be converted to string
+        """
         super().__init__(None)
         self.set_text(text)
 
     def __str__(self):
         return str(self.text)
+
     def __repr__(self):
         return self.__str__()
