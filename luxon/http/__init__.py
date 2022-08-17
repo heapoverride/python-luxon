d8d858e63b6e712e191d396022f680421c1478cd
UnrealSecurity <arran.bishop89@ol.com>
2022-08-17 18:02:23 +0300

    Updated imports

--- luxon/http/__init__.py
+++ luxon/http/__init__.py
@@ -1,5 +1,4 @@
 from luxon.http.app import App
-from luxon.http.handler import Handler
 from luxon.http.route import Route
 from luxon.http.request import Request
 from luxon.http.response import Response
